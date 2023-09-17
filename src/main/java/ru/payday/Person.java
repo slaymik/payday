5695b4d6112c9cdf34e587dc7b8cca68286518a7
slaymik <slava.arapoff@gmail.com>
2023-09-17 18:29:11 +0500

    init commit

--- src/main/java/ru/payday/Person.java
+++
@@ -1,8 +0,0 @@
-package ru.payday;
-
-public class Person extends Human {
-    public Person(String name, int age, String nationality, String gender) {
-        super(name, age, nationality, gender);
-    }
-}
-
--- src/test/java/Person.java
+++
@@ -1,8 +0,0 @@
-import ru.payday.Human;
-
-public class Person extends Human {
-    public Person(String name, int age, String nationality, String gender) {
-        super(name, age, nationality, gender);
-    }
-}
-
